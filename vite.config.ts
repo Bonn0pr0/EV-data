48ffb345d94868f085cb1ccc4dc454b29084859b
Bonn0pr0 <ducbon412@gmail.com>
2025-10-12 13:04:56 +0700

    fix ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:44391',
+        changeOrigin: true,
+        secure: false, // Quan trọng cho HTTPS localhost
+      }
+    }
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+}));
